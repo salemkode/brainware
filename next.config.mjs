1aa46fd87864d2d2e3cdef09996b852d41ddb023
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2025-10-12 15:17:49 +0300

    hh

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,6 @@ import { withContentlayer } from "next-contentlayer";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "pub-5f7cbdfd9ffa4c838e386788f395f0c4.r2.dev",
-      },
-    ],
-  },
 };
 
 export default withContentlayer(nextConfig);
